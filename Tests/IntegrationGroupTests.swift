d777fbdde245a1baa2c51ac20639e39d83b775ec
Eugen Pivovarov <epivovarov@virgilsecurity.com>
2019-05-23 16:00:11 +0300

    Added test on decrypt crash

--- Tests/IntegrationGroupTests.swift
+++ Tests/IntegrationGroupTests.swift
@@ -173,4 +173,59 @@ class IntegrationGroupTests: XCTestCase {
             XCTFail(error.localizedDescription)
         }
     }
+
+    func test2__decrypt__old_session_messages__should_not_crash() {
+        do {
+            // Start group chat
+            let num = 3
+
+            let (cards, chats) = try self.initChat(numberOfParticipants: num)
+
+            let initMsg = try chats.first!.startNewGroupSession(with: [Card](cards.dropFirst()))
+
+            var sessions = [SecureGroupSession]()
+
+            for i in 0..<num {
+                var localCards = cards
+                localCards.remove(at: i)
+
+                let session = try chats[i].startGroupSession(with: localCards, using: initMsg)
+
+                sessions.append(session)
+            }
+
+            // Encrypt plaintext
+            let plainText = UUID().uuidString
+            let message = try sessions.first!.encrypt(string: plainText)
+            let decryptedMessage1 = try sessions.last!.decryptString(from: message)
+            XCTAssert(decryptedMessage1 == plainText)
+
+            // Remove user
+            let experimentalCard = cards.last!
+            let removeCardIds = [experimentalCard.identifier]
+
+            let removeTicket = try sessions.first!.createChangeMembersTicket(add: [], removeCardIds: removeCardIds)
+
+            sessions.removeLast()
+
+            for session in sessions {
+                try session.useChangeMembersTicket(ticket: removeTicket, addCards: [], removeCardIds: removeCardIds)
+            }
+
+            // Return user
+            let addTicket = try sessions.first!.createChangeMembersTicket(add: [experimentalCard], removeCardIds: [])
+
+            for session in sessions {
+                try session.useChangeMembersTicket(ticket: addTicket, addCards: [experimentalCard], removeCardIds: [])
+            }
+
+            let newSession = try chats.last!.startGroupSession(with: cards.dropLast(), using: addTicket)
+            sessions.append(newSession)
+
+            // Decrypt with new session message, encrypted for old session
+            _ = try? sessions.last!.decryptString(from: message)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
 }
