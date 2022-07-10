b9e2e0bfdec65d6b29239fd7dc3a5179e4834567
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2022-07-10 20:11:52 +0300

    Changed sdk-x dependency

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/VirgilSecurity/virgil-sdk-x.git", branch: "develop")
+        .package(url: "https://github.com/VirgilSecurity/virgil-sdk-x.git", branch: "develop-spm-test")
     ],
 
     targets: [
