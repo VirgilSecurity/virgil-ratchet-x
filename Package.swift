8958090a09981df3e92ba1e604b167df2f56b7ff
Sergii Siroshtan <sergey.seroshtan@gmail.com>
2026-06-02 15:17:18 -0400

    Bump virgil-sdk-x to 9.2.3 and virgil-crypto-c to 0.19.1 (stable)

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/VirgilSecurity/virgil-sdk-x.git", exact: "9.2.2"),
-        .package(url: "https://github.com/VirgilSecurity/virgil-crypto-c.git", exact: "0.19.0-rc.14")
+        .package(url: "https://github.com/VirgilSecurity/virgil-sdk-x.git", exact: "9.2.3"),
+        .package(url: "https://github.com/VirgilSecurity/virgil-crypto-c.git", exact: "0.19.1")
     ],
 
     targets: [
